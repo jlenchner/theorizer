2f2e0eeaacf81a49d1748a76bb39d994f05892f7
jlenchner <jon.lenchner@gmail.com>
2025-01-06 19:36:30 -0500

    Create baseUnit.py
    Initial commit

---
+++ baseUnit.py
@@ -0,0 +1,17 @@
+from sympy import *
+class BaseUnit(Symbol):
+
+    def __init__(self, sym):
+        super().__init__()
+        self._sym = Symbol(sym)
+
+    def __str__(self):
+        return self._sym
+
+def base_units(arg_string):
+    syms = symbols(arg_string)
+    base_units = []
+    for sym in syms:
+        bu = BaseUnit(sym.name)
+        base_units.append(bu)
+    return base_units
