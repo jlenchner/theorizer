f5332cafb4c3796424e1ac705f063a35b40bc915
jlenchner <jon.lenchner@gmail.com>
2025-02-03 12:26:26 -0500

    Update constant.py
    Added new member  of this class ._value along with the little bit of infrastructure for supporting it, including the automatic population for a few of the most common constants (c, G, h, h-bar).

--- constant.py
+++ constant.py
@@ -6,8 +6,9 @@ from sympy import *
 class Constant(Variable):  #should be able to automatically pick up units of measure for c, G, h (Plank's constant), h_bar
                            # and others.....
     CONSTANT_TO_UofM_DICT = dict()
+    UNDEFINED = -999
 
-    def __init__(self, name, u_of_m = 1):  #in other words, the default is that the constant is dimensionless
+    def __init__(self, name, u_of_m=1, value=UNDEFINED):  #in other words, the default is that the constant is dimensionless
         self._name = name
         if u_of_m == 1:
             guess = UofM.GuessUofM(measuredQtyDesc=self._name)
@@ -17,8 +18,27 @@ class Constant(Variable):  #should be able to automatically pick up units of mea
                 self._u_of_m = 1
         else:
             self._u_of_m = u_of_m
+        if value == Constant.UNDEFINED and self._u_of_m is not None:
+            self._value = Constant.UNDEFINED  #just to seed it with something
+            self.guessValue()
+        else:
+            self._value = value
+
         Constant.CONSTANT_TO_UofM_DICT.update({name: self._u_of_m})
 
+    def guessValue(self):
+        assumed_u_of_m = UofM.GuessUofM(measuredQtyDesc=self._name)
+        if self._u_of_m == assumed_u_of_m:
+            if self._name == 'c':
+                self._value = 2.998e8
+            elif self._name == 'G':
+                self._value = 6.6743e-11
+            elif self._name == 'h':
+                self._value = 6.62607015e-34
+            elif self._name == 'h-bar':
+                self._value = 1.0545771817e-34
+
+        return self._value
 
     def __str__(self):
         return self._name
