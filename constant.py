0f049a6606c3fe0447f50b5d84c30a7c79305fe9
jlenchner <jon.lenchner@gmail.com>
2025-03-24 22:02:33 -0400

    Documentation overhaul

--- constant.py
+++ constant.py
@@ -1,3 +1,15 @@
+"""The base Constant class. Constants are derived from the Variable class and are in all ways
+    identical to Variables except that they take fixed values, stored in the field _value,
+    and therefore are regarded as constant Variables. Additional common Constants can be added
+    to the UofM class in unitOfMeasure.py: see the parallel lists (at class level) ALL_MEASURED_QUANTITIES
+    and  ALL_UNITS. Note that mathematical constants, and even some physical constants are
+    dimensionless, in which case the _u_of_m can be specified as UofM(1).
+"""
+
+# Author: Jonathan Lenchner (lenchner@us.ibm.com)
+#
+# License: BSD 3-Clause
+
 from unitOfMeasure import *
 from variable import *
 from sympy import *
@@ -12,6 +24,15 @@ class Constant(Variable):  #should be able to automatically pick up units of mea
         return super().__new__(cls, args[0])
 
     def __init__(self, name, u_of_m=1, value=UNDEFINED):  #in other words, the default is that the constant is dimensionless
+        """
+            :param name: Mandatory and should be specified as a string, typically
+                         the string equivalent of the Constant symbol
+            :param u_of_m: A fully constructed UofM. For dimensionless UofMs, pass UofM(1).
+            :param value: The value of the constant in the units specified in u_of_m. For
+                         values of predefined constants refer to the guessValue() method
+                         of this class.
+         """
+
         self._name = name
         if u_of_m == 1:
             guess = UofM.GuessUofM(measuredQtyDesc=self._name)
@@ -30,10 +51,15 @@ class Constant(Variable):  #should be able to automatically pick up units of mea
         Constant.CONSTANT_TO_UofM_DICT.update({name: self._u_of_m})
 
     def guessValue(self):
+        """
+            Guesses the value associated with the constant based on the name. When a new constant is added
+            the associated symbol as a strong should be added to UofM.ALL_MEASURED_QUANTITIES, the associated
+            UofM should be added to UofM.ALL_UNITS and the value (in the supplied units) should be added below.
+        """
         assumed_u_of_m = UofM.GuessUofM(measuredQtyDesc=self._name)
         if self._u_of_m == assumed_u_of_m:
             if self._name == 'c':
-                self._value = 2.998e8
+                self._value = 2.99792458e8
             elif self._name == 'G':
                 self._value = 6.6743e-11
             elif self._name == 'h':
@@ -55,6 +81,10 @@ class Constant(Variable):  #should be able to automatically pick up units of mea
 
 
 def constants(arg_string):
+    """A batch constructor that can be use when relying on guessed UofMs and values. Specify a single
+           string of comma delimited names, e.g., G, c, pi = constants('G,c,pi'),
+           which will create separate Constant objects for G, c and pi.
+       """
     syms =  symbols(arg_string)
     cons = []
     for sym in syms:
